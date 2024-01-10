f7e39ce21f1541bab22355def1136fafe4f2ee62
gaoyuan <m15139007135@163.com>
2024-01-10 19:29:02 +0800

    1/10搜索

--- src/app/(baseLayout)/service/page.tsx
+++ src/app/(baseLayout)/service/page.tsx
@@ -1,14 +1,16 @@
 "use client"
-import { IconButton, InputAdornment, InputBase, Divider } from "@mui/material"
+import { IconButton, InputAdornment, InputBase, Divider, FilterOptionsState } from "@mui/material"
 import SearchIcon from "@mui/icons-material/Search"
 import React from "react"
 import { useRouter } from "next/navigation"
 import { FILE_CONTENT_KEY, HIGH_LIGHT_KEY, OPEN_LIST_KEY } from "../../remove"
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
 import "../service/index.scss"
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { CircularProgress, Typography } from '@mui/material';
+
 const mayBeFind = [
   {
     col: [
@@ -175,22 +177,63 @@ function Service() {
     const updataUrl = `/documentCenter/doc?key=${key}`
     window.open(updataUrl)
   }
-  // 检索下拉提示
-  const suggestions = [
-    { label: "提示1", key: "engineering-structure", },
-    { label: "提示2", key: "engineering-structure", },
-    { label: "提示3", key: "engineering-structure", },
-    { label: "提示4", key: "engineering-structure", },
-    { label: "提示5", key: "engineering-structure", },
-    { label: "提示6", key: "engineering-structure", },
-    { label: "提示7", key: "engineering-structure", }
+  // 搜索栏
+  const [open, setOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState('');
+  const keyword = [
+    { label: "项目管理/创建项目", key: "create-project", title: "申请项目、试用" },
+    { label: "登录/首次登录", key: "first-login", title: "登录、忘记密码、退出登录" },
+    { label: "登录/非首次登录", key: "non-first-login", title: "非首次、登录、忘记密码、退出登录" },
+    { label: "工程管理/构筑物/构筑物", key: "structures-item", title: "工程管理、构筑物" },
+    { label: "工程管理/构筑物/工程结构", key: "engineering-structure", title: "新建、构筑物、工程结构、结构、循环" },
+    { label: "工程管理/构筑物/设计数据", key: "design-data", title: "设计数据、数据" },
+    { label: "工程管理/构筑物/工序管理", key: "process-management", title: "工序、工作量" },
+    { label: "工程管理/单位工程", key: "unit-works", title: "单位工程、解除、关联范围" },
+    { label: "工程管理/工点数据", key: "work-site-data", title: "工点数据、解除、关联范围" },
+    { label: "物资管理/物资进场", key: "material-entry-managements", title: "材料、物资进场、进场、登记" },
+    { label: "物资管理/物资加工", key: "material-processing-managements", title: "材料、物资加工、加工、登记" },
+    { label: "物资管理/物资领用", key: "material-requisition-managements", title: "材料、物资领用、领用、登记" },
+    { label: "试验管理/试验列表", key: "trial-list", title: "材料、试验、登记" },
+    { label: "功能模块/施工计划/施工计划", key: "construction-plan-item", title: "横道图、工期、施工计划、排期" },
+    { label: "功能模块/施工计划/基础数据", key: "basic-data", title: "设计数据" },
+    { label: "功能模块/施工计划/检验批", key: "inspection-batch", title: "检验批、记录表、质量验收" },
+    { label: "功能模块/施工计划/需求计划", key: "demand-planning", title: "物资、物资需求、物资明细、明细" },
+    { label: "用户管理/成员列表", key: "member-list", title: "成员、角色、成员权限、权限" },
+    { label: "竣工管理/竣工资料", key: "completion-data", title: "资料、模板" },
+    { label: "导出管理/导出任务", key: "export-task", title: "检验批、导出任务、导出、下载" },
+    { label: "App/物资进场", key: "material-entry", title: "App、材料、物资进场、进场、登记" },
+    { label: "App/物资试验", key: "material-testing", title: "App、材料、试验、登记" },
+    { label: "App/物资加工", key: "material-processing", title: "App、材料、物资加工、加工、登记" },
+    { label: "App/物资领用", key: "material-requisition", title: "App、材料、物资领用、领用、登记" },
+    { label: "App/施工日志", key: "construction-log", title: "App、施工日志、日志" },
+    { label: "App/个人中心", key: "personal-center", title: "App、项目信息、账号" },
+    { label: "App/设置", key: "set-up", title: "App、退出登录" },
   ];
   // 搜索跳转
   function handleSearch(selectedOption: { label: string; key: string } | null) {
     if (selectedOption) {
       const { key } = selectedOption;
-      const searchUrl = `/documentCenter/doc?key=${key}`;
+      const searchUrl = `/documentCenter/doc?key=${key}`
       window.open(searchUrl);
+      setTimeout(() => {
+        setInputValue('');
+      }, 100); 
+    }
+    
+  }
+  function CustomNoOptions() {
+    return (
+      <Typography>未找到相关内容</Typography>
+    );
+  }
+  // 搜索框下拉提示的开关
+  function handleInputChange(event: any, value: any) {
+    setInputValue(value);
+    if (value) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+      setInputValue('');
     }
   }
   return (
@@ -198,33 +241,44 @@ function Service() {
       <main className="mt-16 w-full mx-auto service dropdownContainer">
         {/* 搜索 */}
         <div className="banner h-[31.25rem] bg-railway_deep_red overflow-hidden text-center w-full">
-          <div className="max-w-[1920px] mx-auto w-full flex flex-col justify-center gap-y-8 items-center h-full mt-3">
+          <div className="max-w-[1920px] mx-auto w-full flex flex-col justify-center gap-y-8 items-center h-full">
             <h2 className="text-center text-white text-[40px] ">欢迎使用服务支持</h2>
-            {/* <InputBase
-              className="w-[580px] bg-[#f8fafb] border rounded-md px-2 mx-auto h-12"
-              placeholder="搜索服务与支持"
-              size="medium"
-              onChange={handleInputChange}
-              startAdornment={
-                <InputAdornment position="start">
-                  <IconButton
-                    type="button"
-                    edge="end"
-                    sx={{ p: "10px" }}
-                    aria-label="search"
-                    disableRipple>
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
-              }
-            /> */}
             <Autocomplete
               disablePortal
               id="combo-box-demo"
-              options={suggestions}
-              sx={{ width: 580, background: "#f8fafb" ,borderRadius: '8px'}}
-              renderInput={(params) => <TextField {...params} label="欢迎使用服务支持" />}
+              options={keyword}
+              filterOptions={function (options: { label: string; key: string; title: string; }[]): { label: string; key: string; title: string; }[] {
+                const mate = keyword.filter((suggestion) =>
+                  suggestion.title.toLowerCase().includes(inputValue)
+                );
+                return mate
+              }}
+              getOptionLabel={function (mate) {
+                return mate.label
+              }}
+              sx={{ width: 580, background: "#f8fafb", borderRadius: '8px' }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="欢迎使用服务支持"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton className="iconBtn">
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              )}
               onChange={(event, value) => handleSearch(value)}
+              noOptionsText={<CustomNoOptions />}
+              open={open}
+              onClose={() => setOpen(false)}
+              onInputChange={handleInputChange}
+              inputValue={inputValue}
             />
           </div>
         </div>
